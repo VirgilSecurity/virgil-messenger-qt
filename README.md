30c0847f2c8c30a2ac68f9018bc27814d6584e91
Oleksandr Nemchenko <avn@nemchenko.net>
2019-12-18 14:46:48 +0200

    [android] Prepare Android deploy

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# demo-iotkit-qt
+# Qt/QML Demo for Virgil IoTKIT Usage
+<a href="https://developer.virgilsecurity.com/docs"><img width="230px" src="https://cdn.virgilsecurity.com/assets/images/github/logos/iotkit/IoTKit.png" align="left" hspace="1" vspace="3"></a>
+
+## Introduction
+[Virgil Security](https://virgilsecurity.com) provides a set of APIs for adding security to any application or IoT device.
+
+[Virgil IoTKit](https://github.com/virgilsecurity/virgil-iotkit/) is a C library for connecting IoT devices to the Virgil IoT Security Platform. IoTKit helps you easily add security to your IoT devices at any lifecycle stage for securely provisioning and authenticating devices, updating firmware and TrustLists, and exchanging messages using any transport protocols.
+
+Demo for Virgil IoTKIT is the Virgil IoTKIT usage example based on C++ and Qt/QML library.
+
+## Prerequisites
+To compile Demo for Virgil IoTKIT the following components are required:
+- C99 for Virgil IoTKIT.
+- C++14 for this demo.
+- CMake v3.11 or higher, for project building
+- GCC or another toolchain for C/C++ compile
+- Qt for QML usage.
+
+Virgil IoTKIT provides `cmake/android/qt-android-mk-apk.cmake` CMake file. It allows you to deploy application for Android
+by using `androiddeployqt` tool provided by Qt library. To use it you need to install Android SDK, NDK and Qt with Android components.
+
+## Android Build
+`deploy_android.sh` script builds, deploys and starts demo application on Android device. There are several constants that are described
+in this script.
+
+## License
+
+This demo and Virgil IoTKIT is released under the [3-clause BSD License](LICENSE).
+
+<div id='support'/>
+
+## Support
+Our developer support team is here to help you. Find more information on our [Help Center](https://help.virgilsecurity.com/).
+
+You can find us on [Twitter](https://twitter.com/VirgilSecurity) or send us an email at support@VirgilSecurity.com.
+
+Also, get extra help from our support team on [Slack](https://virgilsecurity.com/join-community).
---
+++ deploy_android.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+USER_DIR=${HOME}
+
+# Target name
+TARGET=iotkit-qt-example
+
+# Build type - Release, Debug
+BUILD_TYPE=Debug
+
+# Make directory
+MAKE_DIR=${BUILD_TYPE}/shell.Android
+
+# Path to the directory with Android SDK
+ANDROID_SDK=${USER_DIR}/Android/SDK
+
+# Path to the directory with Android NDK
+ANDROID_NDK=${ANDROID_SDK}/ndk/20.1.5948944
+
+# Toolchain C and C++ compilers
+ANDROID_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang
+ANDROID_CXX_COMPILER=${ANDROID_C_COMPILER}++
+
+# Android architecture
+ANDROID_ABI=armeabi-v7a
+
+# Qt Toolchain for Android
+QT_TOOLCHAIN=${USER_DIR}/Qt/5.12.6/android_armv7
+
+# Android package name / activity name. You can find both in the generated manifest file (AndroidManifest.xml in the
+# ${target}-apk-build directory after build)
+ANDROID_APP=org.qtproject.example.iotkit_qt_example/org.qtproject.qt5.android.bindings.QtActivity
+
+# C++ library type
+ANDROID_STL=c++_shared
+
+# Android platform (API level)
+ANDROID_PLATFORM=24
+
+# CMake system name
+CMAKE_SYSTEM_NAME=ANDROID
+
+# Application description
+APP_DESCRIPTION="Demo"
+
+CMAKE_ARGS=" -DANDROID_QT=ON"
+CMAKE_ARGS+=" -DANDROID_ABI=${ANDROID_ABI}"
+CMAKE_ARGS+=" -DANDROID_NDK=${ANDROID_NDK}"
+CMAKE_ARGS+=" -DANDROID_SDK=${ANDROID_SDK}"
+CMAKE_ARGS+=" -DANDROID_STL=${ANDROID_STL}"
+CMAKE_ARGS+=" -DANDROID_PLATFORM=${ANDROID_PLATFORM}"
+CMAKE_ARGS+=" -DCMAKE_ANDROID_ARCH_ABI=${ANDROID_ABI}"
+CMAKE_ARGS+=" -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+CMAKE_ARGS+=" -DCMAKE_CXX_COMPILER=${ANDROID_CXX_COMPILER}"
+CMAKE_ARGS+=" -DCMAKE_C_COMPILER=${ANDROID_C_COMPILER}"
+CMAKE_ARGS+=" -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH"
+CMAKE_ARGS+=" -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=BOTH"
+CMAKE_ARGS+=" -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
+CMAKE_ARGS+=" -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=BOTH"
+CMAKE_ARGS+=" -DCMAKE_PREFIX_PATH=${QT_TOOLCHAIN}"
+CMAKE_ARGS+=" -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}"
+CMAKE_ARGS+=" -DCMAKE_SYSTEM_VERSION=${ANDROID_PLATFORM}"
+CMAKE_ARGS+=" -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake"
+CMAKE_ARGS+=" -DANDROID_APP_DESCRIPTION=${APP_DESCRIPTION}"
+CMAKE_ARGS+=" -DQT_TOOLCHAIN=${QT_TOOLCHAIN}"
+
+echo
+echo ==
+echo ==  CMake arguments :
+echo ==
+echo
+
+echo ${CMAKE_ARGS} | tr " " "\n"
+
+echo
+echo ==
+echo ==  Prepare build system in directory ${MAKE_DIR}
+echo ==
+echo
+
+rm -rf ${MAKE_DIR}
+mkdir -p ${MAKE_DIR}
+cmake -S . -B ${MAKE_DIR} ${CMAKE_ARGS}
+
+echo
+echo ==
+echo ==  Build target ${TARGET} and its deployment ${TARGET}-apk-install
+echo ==
+echo
+
+cmake --build ${MAKE_DIR} --target ${TARGET}-apk-install -- -j 4
+
+echo
+echo ==
+echo ==  Run on Android device
+echo ==
+echo
+
+${ANDROID_SDK}/platform-tools/adb shell am start -n ${ANDROID_APP}
