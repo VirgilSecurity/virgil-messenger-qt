44037adc40cd0b02e07fa4e8c7ded0d976db3c17
lantaris <edik.ponomarenko@mail.ru>
2020-05-15 09:28:30 +0300

    Fix env for webdriver

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -9,21 +9,20 @@ source ${SCRIPT_FOLDER}/ish/common.sh
 PLATFORM=linux-g++
 BUILD_DIR=${PROJECT_DIR}/${BUILD_TYPE}/${TOOL_NAME}.${PLATFORM}/
 LINUX_QMAKE="${QT_SDK_DIR}/gcc_64/bin/qmake"
-QMAKE_PARAMS="${QMAKE_PARAMS:-''}"
+QMAKE_PARAMS="${QMAKE_PARAMS:-" "}"
 
 #***************************************************************************************
 
 print_title
 
-prepare_libraries
+#prepare_libraries
 
 new_dir ${BUILD_DIR}
 
 print_message "Build application bundle"
 
 pushd "${BUILD_DIR}"
-
-    ${LINUX_QMAKE} -config ${BUILD_TYPE} ${PROJECT_DIR} VERSION="${VERSION}" ${QMAKE_PARAMS}
+    ${LINUX_QMAKE} -config ${BUILD_TYPE} ${PROJECT_DIR} ${QMAKE_PARAMS} VERSION="${VERSION}" 
 
     make -j10
 
