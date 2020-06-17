a7987b1aa2c041e9812e7ce65e569e0c34a1756e
Roman Kutashenko <kutashenko@gmail.com>
2020-06-17 15:43:58 +0300

    Prevent connection lost on desktops

--- src/VSQApplication.cpp
+++ src/VSQApplication.cpp
@@ -147,6 +147,7 @@ VSQApplication::onApplicationStateChanged(Qt::ApplicationState state) {
     static bool _deactivated = false;
     qDebug() << state;
 
+#if VS_PUSHNOTIFICATIONS
     if (Qt::ApplicationInactive == state) {
         _deactivated = true;
         m_messenger.setStatus(VSQMessenger::MSTATUS_UNAVAILABLE);
@@ -158,6 +159,7 @@ VSQApplication::onApplicationStateChanged(Qt::ApplicationState state) {
             this->m_messenger.checkState();
         });
     }
+#endif // VS_ANDROID
 }
 
 
