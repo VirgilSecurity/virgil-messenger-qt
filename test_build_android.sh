cf8a73e419ad175e229a3b99fad2fb2d6ae56047
lantaris <edik.ponomarenko@mail.ru>
2020-10-01 12:46:20 +0300

    Add templated multicustomers

--- test_build_android.sh
+++ test_build_android.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-rm -rf build
-mkdir -p build 
-pushd build
+rm -rf build-virgil
+mkdir -p build-virgil
+pushd build-virgil
+ cmake -DVS_PLATFORM="android" ..
+ make 
+ make apk
+popd
+
+rm -rf build-area52
+mkdir -p build-area52
+pushd build-area52
  cmake -DVS_PLATFORM="android" ..
  make 
  make apk
