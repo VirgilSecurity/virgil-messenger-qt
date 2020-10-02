8e58cb68e3c21d389cd81ef4e93a818f165e06ad
lantaris <edik.ponomarenko@mail.ru>
2020-10-02 10:35:25 +0300

    Prepare for MacOS

--- test_build_android.sh
+++ test_build_android.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-rm -rf build-virgil
-mkdir -p build-virgil
-pushd build-virgil
- cmake -DVS_PLATFORM="android" ..
- make 
- make apk
-popd
+#rm -rf build-virgil
+#mkdir -p build-virgil
+#pushd build-virgil
+# cmake -DVS_PLATFORM="android" -DVS_CUSTOMER="Virgil" ..
+# make 
+# make apk
+#popd
 
 rm -rf build-area52
 mkdir -p build-area52
 pushd build-area52
- cmake -DVS_PLATFORM="android" ..
+ cmake -DVS_PLATFORM="android" -DVS_CUSTOMER="Area52" ..
  make 
  make apk
 popd
