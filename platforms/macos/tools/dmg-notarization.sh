7aaa93a6e8b8b9322c4345052402231944190c50
lantaris <edik.ponomarenko@mail.ru>
2020-12-09 16:05:58 +0300

    Rework MacOS notarization tool

--- platforms/macos/tools/dmg-notarization.sh
+++ platforms/macos/tools/dmg-notarization.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-set -o errtrace
+set -e
 #
 #   Global variables
 #
 SCRIPT_FOLDER="$(cd $(dirname "$0") && pwd)"
 
 #***************************************************************************************
-function print_message() {
+print_message() {
     echo
     echo "===================================="
     echo "=== ${1}"
     echo "===================================="
+}
+
+print_notarize_status() {
+	echo "============================================"    
+	echo "=== Get status state:	$(echo "${INFO_OUTPUT}" | jq -r '."success-message"')"
+	echo "=== Status message:	$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Status Message"')"
+	echo "=== Status URL:		$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."LogFileURL"')"
+	echo "=== Date:			$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Date"')"
+	echo "=== Status:		$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Status"')"	
+	echo "=== Status code:		$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Status Code"')"
+	echo "============================================"
 }
 
 ############################################################################################
@@ -57,15 +68,19 @@ done
 function notarize_dmg() {
 	print_message "Send Application for Apple's notarization"
 
-	NOTARIZE_OUTPUT=$(xcrun altool -t osx -f "${DMG_FILE}" --primary-bundle-id "${PKG_IDENTIFIER}" --notarize-app --username ${USER_NAME} -p ${PASS} 2>&1)
-	
-	NOTARIZE_ID=$(echo ${NOTARIZE_OUTPUT} | tr -d "\n" | grep -F 'No errors uploading' | awk -F 'RequestUUID' '{print $2}' | awk -F ' ' '{print $2}')
+	NOTARIZE_OUTPUT=$(xcrun altool -t osx -f "${DMG_FILE}" --output-format json --primary-bundle-id "${PKG_IDENTIFIER}" --notarize-app --username ${USER_NAME} -p ${PASS})
+	NOTARIZE_ID="$(echo "${NOTARIZE_OUTPUT}" | jq -r '."notarization-upload"."RequestUUID"')"
 
-	if [ "${NOTARIZE_ID}" == "" ]; then
+	echo "============================================"
+    	echo "=== Tool version: 	$(echo "${NOTARIZE_OUTPUT}" | jq -r '."tool-version"')"
+	echo "=== Upload result:	$(echo "${NOTARIZE_OUTPUT}" | jq -r '."success-message"')"
+	echo "=== OS version:		$(echo "${NOTARIZE_OUTPUT}" | jq -r '."os-version"')"
+	echo "=== Notarization ID:	${NOTARIZE_ID}"
+	echo "============================================"	
+
+	if [ "${NOTARIZE_ID}" == "" ] || [ "${NOTARIZE_ID}" == "null" ]; then
 	    echo "Error notarization"
-	    echo "${NOTARIZE_OUTPUT}"
-	else
-    	    echo "NOTARIZE_ID = ${NOTARIZE_ID}"		    
+	    exit 127
 	fi 
 
 	print_message "Get result of notarization"
@@ -74,34 +89,30 @@ function notarize_dmg() {
 	for count in $(seq 1 300); do
 		echo "Wait .. ${count} of 300"
 		sleep 10s
-		INFO_OUTPUT=$(xcrun altool --notarization-info "${NOTARIZE_ID}" --username ${USER_NAME} -p ${PASS} 2>&1 | tr -d "\n")
+		INFO_OUTPUT=$(xcrun altool --output-format json --notarization-info "${NOTARIZE_ID}" --username ${USER_NAME} -p ${PASS})
+		NOTARIZE_STATUS_CODE="$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Status Code"')"
+		NOTARIZE_STATUS="$(echo "${INFO_OUTPUT}" | jq -r '."notarization-info"."Status"')"		
+		print_notarize_status
 
-		if echo ${INFO_OUTPUT} | grep -q -F 'Status Message: Package Approved'; then
-			NOTARIZATION_DONE="true"
-			break
+		if [ "${NOTARIZE_STATUS_CODE}" == "null" ] || [ "${NOTARIZE_STATUS}" == "in progress" ]; then
+		  continue
 		fi
-
-		if echo ${INFO_OUTPUT} | grep -q -F 'Status Message: Package Invalid'; then
-			echo "${INFO_OUTPUT}"
-			exit 1
+		if [ "${NOTARIZE_STATUS_CODE}" != "0" ]; then
+		    exit 127
+		else
+		    print_message "Staple result of the notarization"
+		    STAMPLE_OUTPUT=$(xcrun stapler staple -v "${DMG_FILE}" 2>&1 | tr -d "\n")
+		    if echo ${STAMPLE_OUTPUT} | grep -q -F 'The staple and validate action worked!'; then
+		        echo ""
+		        exit 0
+		    else
+		        echo "${STAMPLE_OUTPUT}"
+		        exit 127
+		    fi		    
 		fi
 	done
+	print_message "Notarization time out"
 
-	if [ "${NOTARIZATION_DONE}" == "false" ]; then
-		echo "${INFO_OUTPUT}"
-		exit 1
-	fi
-
-	print_message "Staple result of the notarization"
-
-	STAMPLE_OUTPUT=$(xcrun stapler staple -v "${DMG_FILE}" 2>&1 | tr -d "\n")
-
-	if echo ${STAMPLE_OUTPUT} | grep -q -F 'The staple and validate action worked!'; then
-		echo ""
-	else
-		echo "${STAMPLE_OUTPUT}"
-		exit 1
-	fi
 }
 
 #***************************************************************************************
