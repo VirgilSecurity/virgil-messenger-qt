a50bd185b907c091b81a0c5fb9d89f95fb7f63e8
lantaris <edik.ponomarenko@mail.ru>
2021-01-11 18:10:21 +0300

    Temp commit(merge)

--- scripts/build-ios-archive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_FOLDER="$(cd "$(dirname "$0")" && pwd)"
-
-pushd  ${SCRIPT_FOLDER}/../release/qmake.ios
-
-xcodebuild -project VirgilMessenger.xcodeproj -scheme VirgilMessenger -sdk iphoneos -configuration Release -destination generic/platform=iOS archive -archivePath artifacts/VirgilMessenger.xcarchive
-
-popd
