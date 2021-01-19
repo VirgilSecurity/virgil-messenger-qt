b1d5030542c2d731e3afdfca04e90117c3392ab8
lantaris <edik.ponomarenko@mail.ru>
2021-01-19 10:12:21 +0200

    Move storyboard to project

--- examples/scripts/test_build_ios.sh
+++ examples/scripts/test_build_ios.sh
@@ -8,17 +8,17 @@ pushd ${PROJECT_FOLDER}
 
   rm -rf build-sim
   mkdir -p build-sim
-  
+
   rm -rf build
   mkdir -p build
 
   cmake -S. -Bbuild-sim -GXcode -DVS_PLATFORM="ios" -DVS_IOS_SIMULATOR=TRUE -DVS_DEVELOPMENT_TEAM="JWNLQ3HC5A"
-  cmake --build build-sim --config Debug 
-  
+  cmake --build build-sim --config Debug
+
   cmake -S. -Bbuild -GXcode -DVS_PLATFORM="ios" -DVS_DEVELOPMENT_TEAM="JWNLQ3HC5A"
   cmake --build build --config Release
-#  cmake --build build --config Release --target xcarchive
+  cmake --build build --config Release --target xcarchive
 #  cmake --build build --config Release --target upload_testflight
 
-popd  
+popd
 
