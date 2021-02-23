3be5e49bb4291c3eee0524fa87cf311b4a2eea58
lantaris <edik.ponomarenko@mail.ru>
2021-02-23 13:45:21 +0300

    Windows support

---
+++ examples/scripts/test_build_windows.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_FOLDER="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_FOLDER="${SCRIPT_FOLDER}/../.."
+
+pushd ${PROJECT_FOLDER}
+
+  rm -rf build
+  mkdir -p build 
+
+  pushd build
+   cmake -DVS_PLATFORM="windows" -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-mingw64.cmake ..
+   make -j10
+   make deploy
+  popd
+popd
