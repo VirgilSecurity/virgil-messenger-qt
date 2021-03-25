e0824b928839b06498db0ab53c7dc981eda1fdfb
fpohtmeh <fpohtmehgithub@gmail.com>
2021-03-25 22:35:33 +0200

    Added initial version for android preview

--- platforms/android/src/org/virgil/utils/DocumentInteraction.java
+++ platforms/android/src/org/virgil/utils/DocumentInteraction.java
@@ -1,6 +1,10 @@
 package org.virgil.utils;
 
+import org.qtproject.qt5.android.QtNative;
+
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 public class DocumentInteraction
@@ -8,6 +12,8 @@ public class DocumentInteraction
     private static final String TAG = "DocumentInteraction";
 
     public static void viewFile(Context context, String filePath) {
-        Log.d(TAG, "test");
+        Uri uri = Uri.parse(filePath);
+        Intent viewIntent = new Intent(Intent.ACTION_VIEW, uri);
+        QtNative.activity().startActivity(viewIntent);
     }
 }
