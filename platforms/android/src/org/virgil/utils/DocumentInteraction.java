305f0eca75e307ad91c910a143e1e9fddb7a4314
fpohtmeh <fpohtmehgithub@gmail.com>
2021-03-25 23:28:00 +0200

    Improved android file preview

--- platforms/android/src/org/virgil/utils/DocumentInteraction.java
+++ platforms/android/src/org/virgil/utils/DocumentInteraction.java
@@ -6,14 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import androidx.core.content.FileProvider;
+import java.io.File;
 
 public class DocumentInteraction
 {
     private static final String TAG = "DocumentInteraction";
 
     public static void viewFile(Context context, String filePath) {
-        Uri uri = Uri.parse(filePath);
-        Intent viewIntent = new Intent(Intent.ACTION_VIEW, uri);
-        QtNative.activity().startActivity(viewIntent);
+        File file = new File(filePath);
+        try {
+            Uri uri = FileProvider.getUriForFile(context, "com.virgilsecurity.android.virgil.provider", file);
+            Intent viewIntent = new Intent(Intent.ACTION_VIEW, uri);
+            QtNative.activity().startActivity(viewIntent);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "The selected file can't be opened: " + file.toString());
+            Log.e(TAG, "Error: " + e.toString());
+        }
     }
 }
